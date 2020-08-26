134fcdd0460af311436ae72146eb91a3ddfb9ea9
Allidjinou Michée Josué <allidjinoumichee@outlook.fr>
2020-08-26 16:52:52 +0000

    Fix todo name overflow

--- imports/ui/Todo.js
+++ imports/ui/Todo.js
@@ -6,11 +6,9 @@ import {Todos} from './../api/todos';
 export default class Todo extends React.Component {
   render() {
     return (
-      <div key={this.props.todo._id} class="todo">
-        <div>
-          <h3 class="todo__message">{this.props.todo.name}</h3>
-        </div>
-        <div class="todo__actions">
+      <div key={this.props.todo._id} className="todo">
+        <h3 className="todo__message">{this.props.todo.name}</h3>
+        <div className="todo__actions">
           <button className="button">Done</button>
           <button onClick={() => {
             Todos.remove(this.props.todo._id)
